96e22fedb57f79b1f665c842796e6e60243cabc0
Pranali Lotake <60060241+Pranali-22@users.noreply.github.com>
2022-09-05 20:07:14 +0530

    Added SampleData.java

---
+++ SampleData.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.Bridgelabz;
+
+/**
+ * @author Dell
+ *
+ */
+public class SampleData {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World...");
+		
+		//non primitive Data type
+		String name="Pranali";
+		System.out.println(name);
+		
+		//primitive data type
+		int a=10;
+		int b=20;
+		System.out.println("a="+a+"\nb="+b);
+		int sum=a+b;
+		System.out.println(sum);
+	}
+
+}
